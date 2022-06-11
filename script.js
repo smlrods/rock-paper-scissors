bd130268a1413619f0ff187767396baac03193d9
Samuel <m.samuelrodrigues.98@gmail.com>
2022-06-10 22:35:41 -0300

    Adiciona função game e atualiza funcao playRound
    Funcao game() calcula o score e decide o vencedor.
    Modifica o return da funcao playRound, nao retornando mais uma string,
    mas sim um valor boolean.

--- script.js
+++ script.js
@@ -35,66 +35,122 @@ function playRound(playerSelection, computerSelection) {
     {
         if (computerSelection == 'ROCK')
         {
-            return "It's a tie! Rock ties with Rock";
+             console.log("It's a tie! Rock ties with Rock");
+             return 0;
         } 
         else if (computerSelection === 'PAPER') 
         {
-            return "You lost! Paper beats Rock";
+             console.log("You lost! Paper beats Rock");
+             return false;
         } 
         else if (computerSelection === 'SCISSORS') 
         {
-            return "You won! Rock beats Scissors";
+             console.log("You won! Rock beats Scissors");
+             return true;
         } 
         else 
         {
-            return 'Invalid input!';
+            return undefined;
         }
     } 
     else if (playerSelection === 'PAPER')
     {
         if (computerSelection == 'ROCK')
         {
-            return "You won! Paper beats Rock";
+             console.log("You won! Paper beats Rock");
+             return true;
         }
         else if (computerSelection === 'PAPER')
         {
-            return "It's a tie! Paper ties with Paper";
+             console.log("It's a tie! Paper ties with Paper");
+             return 0;
         }
         else if (computerSelection === 'SCISSORS')
         {
-            return "You lost! Scissors beats Paper";
+             console.log("You lost! Scissors beats Paper");
+             return false;
         }
         else
         {
-            return 'Invalid input!';
+            return undefined;
         }
     } 
     else if (playerSelection === 'SCISSORS')
     {
         if (computerSelection == 'ROCK')
         {
-            return "You lost! Rock beats Scissors";
+             console.log("You lost! Rock beats Scissors");
+             return false;
         }
         else if (computerSelection === 'PAPER')
         {
-            return "You won! Scissors beats Paper";
+             console.log("You won! Scissors beats Paper");
+             return true;
         }
         else if (computerSelection === 'SCISSORS')
         {
-            return "It's a tie! Scissors ties with Scissors";
+             console.log("It's a tie! Scissors ties with Scissors");
+             return 0;
         } 
         else
         {
-            return 'Invalid input!';
+            return undefined;
         }
     } 
     else
     {
-        return 'Invalid input!';
+        return undefined;
     }
 }
 
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    let playerSelection;
+    let computerSelection;
+    let result;
 
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+    for (i = 0; i < 5; i++)
+    {
+        console.log(`Round: ${i+1}`);
+        playerSelection = prompt('Rock, Paper or Scissors?');
+        computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection);
+        if (result === undefined)
+        {
+            console.log('Invalid. Try again:');
+            i--;
+        }
+        else if (result === true)
+        {
+            ++scorePlayer;
+            console.log(`Score:\n Player: ${scorePlayer}\n Computer: ${scoreComputer}`);
+        }
+        else if (result === false)
+        {
+            ++scoreComputer;
+            console.log(`Score:\n Player: ${scorePlayer}\n Computer: ${scoreComputer}`);
+        }
+        else if (result === 0)
+        {
+            console.log(`Score:\n Player: ${scorePlayer}\n Computer: ${scoreComputer}`);
+        }
+        else
+        {
+            console.error('Error');
+        }
+    }
+
+    if (scorePlayer > scoreComputer) 
+    {
+        console.log("You Won!");
+    }
+    else if (scorePlayer < scoreComputer)
+    {
+        console.log("You Lost");
+    }
+    else
+    {
+        console.log("It's a tie!");
+    }
+}
